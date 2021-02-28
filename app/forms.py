5bff5eae355e5db153195ed1dcc6d80c2bc76f39
Michel Davies <micheldavies0288@gmail.com>
2021-02-27 20:58:09 -0500

    Completed

--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,6 @@
 
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileRequired, FileAllowed
-from wtforms import StringField
-from wtforms.validators import DataRequired
+
 class UploadForm(FlaskForm):
     Image =FileField(validators=[FileRequired(), FileAllowed(['jpg', 'png'],"Only images are accepted")])
